0dab3e4d2e501bf7f3614deeeb4894bbbf7a85da
BrovchenkoDmitriy <99586855+BrovchenkoDmitriy@users.noreply.github.com>
2022-10-21 10:51:03 +0900

    some changes to UI. - add WeekWeatherItemSecondBinding and replace WeekWeatherItemBinding on WeekWeatherItemSecondBinding. - add some resource to color.

--- app/src/main/java/com/example/testretrofitapp/presentation/weekForecast/weekForecastRecyclerView/WeatherViewHolder.kt
+++ app/src/main/java/com/example/testretrofitapp/presentation/weekForecast/weekForecastRecyclerView/WeatherViewHolder.kt
@@ -2,7 +2,7 @@ package com.example.testretrofitapp.presentation.weekForecast.weekForecastRecycl
 
 import androidx.recyclerview.widget.RecyclerView
 import com.example.testretrofitapp.databinding.WeekWeatherItemBinding
+import com.example.testretrofitapp.databinding.WeekWeatherItemSecondBinding
 
-class WeatherViewHolder (val binding: WeekWeatherItemBinding):RecyclerView.ViewHolder(binding.root){
-
-}
+class WeatherViewHolder (val binding: WeekWeatherItemSecondBinding):RecyclerView.ViewHolder(binding.root)
+//заменили с WeekWeatherItemBinding на WeekWeatherItemSecondBinding
