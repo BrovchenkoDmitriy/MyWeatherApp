930f8f12bc6ddb0ad9b1046c79a25812fe7ccb3d
BrovchenkoDmitriy <99586855+BrovchenkoDmitriy@users.noreply.github.com>
2022-12-15 16:03:51 +0900

    Refactor of code. add progressBar and crossFadeAnimation to MainWeatherFragment.kt

--- app/src/main/java/com/example/myweatherapp/presentation/currentWeather/MainWeatherFragment.kt
+++ app/src/main/java/com/example/myweatherapp/presentation/currentWeather/MainWeatherFragment.kt
@@ -1,5 +1,7 @@
 package com.example.myweatherapp.presentation.currentWeather
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.icu.text.SimpleDateFormat
 import android.os.Bundle
@@ -69,6 +71,18 @@ class MainWeatherFragment : Fragment() {
         Log.d("TAG", "onViewCreated")
         val lat = 50.2997427
         val lon = 127.5023826
+        viewModel.clearLiveData()
+        getWeather(lat, lon)
+        setupRecyclerView()
+        initData()
+
+        binding.loadWeatherButton.setOnClickListener {
+            getWeather(lat, lon)
+            initData()
+        }
+    }
+
+    private fun getWeather(lat: Double, lon: Double) {
         viewModel.getWeather(
             lat,
             lon,
@@ -77,35 +91,16 @@ class MainWeatherFragment : Fragment() {
             WeatherApp.UNITS,
             WeatherApp.LANG
         )
-        setupRecyclerView()
-        initData()
-        binding.loadWeatherButton.setOnClickListener {
-            viewModel.getWeather(
-                lat,
-                lon,
-                WeatherApp.EXCLUDE,
-                WeatherApp.APPID,
-                WeatherApp.UNITS,
-                WeatherApp.LANG
-            )
-            initData()
-        }
     }
 
     private fun initData() {
         Log.d("TAG", "startInitData")
-        binding.clCurrentWeather.visibility = View.GONE
-        binding.rvFurtherWeek.visibility = View.GONE
-        binding.progressBar.visibility = View.VISIBLE
         viewModel.currentWeatherEntity.observe(viewLifecycleOwner) {
             it?.let {
                 val format = SimpleDateFormat("dd MMMM, HH:mm", Locale.getDefault())
                 val currentTime = System.currentTimeMillis()
                 val currentTimeString = format.format(currentTime).toString() + "\n" + it.dt
                 val feelLikeTemp = "Ощущается как " + it.feelsLike
-                binding.progressBar.visibility = View.GONE
-                binding.clCurrentWeather.visibility = View.VISIBLE
-                binding.rvFurtherWeek.visibility = View.VISIBLE
 
                 with(binding) {
                     bindImage(ivWeatherIcon, it.icon)
@@ -114,6 +109,7 @@ class MainWeatherFragment : Fragment() {
                     tvCurrentTime.text = currentTimeString
                     tvFeelsLikeTemp.text = feelLikeTemp
                 }
+                crossFadeAnimation()
             }
         }
         viewModel.weekWeatherEntity.observe(viewLifecycleOwner) {
@@ -128,6 +124,48 @@ class MainWeatherFragment : Fragment() {
                 else View.GONE
             }
         }
+    }
+
+    private fun crossFadeAnimation(){
+        val shortAnimationDuration = resources.getInteger(android.R.integer.config_longAnimTime)
+        binding.progressBar.visibility = View.GONE
+        binding.clCurrentWeather.visibility = View.VISIBLE
+        binding.rvFurtherWeek.visibility = View.VISIBLE
+        binding.clCurrentWeather.apply {
+            // Set the content view to 0% opacity but visible, so that it is visible
+            // (but fully transparent) during the animation.
+            alpha = 0f
+            visibility = View.VISIBLE
+
+            // Animate the content view to 100% opacity, and clear any animation
+            // listener set on the view.
+            animate()
+                .alpha(1f)
+                .setDuration(shortAnimationDuration.toLong())
+                .setListener(null)
+        }
+        binding.rvFurtherWeek.apply {
+            alpha = 0f
+            visibility = View.VISIBLE
+
+            animate()
+                .alpha(1f)
+                .setDuration(shortAnimationDuration.toLong())
+                .setListener(null)
+        }
+
+        // Animate the loading view to 0% opacity. After the animation ends,
+        // set its visibility to GONE as an optimization step (it won't
+        // participate in layout passes, etc.)
+        binding.progressBar.animate()
+            .alpha(0f)
+            .setDuration(shortAnimationDuration.toLong())
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.progressBar.visibility = View.GONE
+                }
+            })
+
     }
 
     private fun bindImage(imgView: ImageView, icon: String) {
