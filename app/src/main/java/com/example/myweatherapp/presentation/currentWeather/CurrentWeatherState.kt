1169a589340b7f4839809442e321422cb9d3ae16
BrovchenkoDmitriy <99586855+BrovchenkoDmitriy@users.noreply.github.com>
2023-10-16 16:39:42 +0900

    State UDF
    - refactor CurrentWeatherState.kt

--- app/src/main/java/com/example/myweatherapp/presentation/currentWeather/CurrentWeatherState.kt
+++ app/src/main/java/com/example/myweatherapp/presentation/currentWeather/CurrentWeatherState.kt
@@ -1,14 +1,7 @@
 package com.example.myweatherapp.presentation.currentWeather
 
-import com.example.myweatherapp.domain.CurrentWeatherEntity
-import com.example.myweatherapp.domain.DailyWeatherEntity
-import com.example.myweatherapp.domain.HourlyWeatherEntity
-
 sealed class CurrentWeatherState
-class Success(
-    val currentWeatherEntity: CurrentWeatherEntity,
-    val hourlyWeatherEntity: List<HourlyWeatherEntity>
-) : CurrentWeatherState()
+object Success : CurrentWeatherState()
 
 object Loading : CurrentWeatherState()
 class Error(val error: String) : CurrentWeatherState()
